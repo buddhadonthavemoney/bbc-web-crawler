c91c348a75956733ec57590936e3f72e0b95f358
buddha gautam <buddhagautam231@gmail.com>
2022-04-16 20:17:43 +0545

    code refactoring

--- bbc.py
+++ bbc.py
@@ -9,12 +9,37 @@ from crawl import crawl_sub_categories, get_headlines
 class BBC:
     def __init__(self):
         self.base_url = "https://www.bbc.com/"
-        self.adjacencyList = dict()
-        # adjacencyList is a graph represented using a list
         self.home_html = requests.get(self.base_url)
         self.soup = BeautifulSoup(
             self.home_html.text, 'html.parser'
         )
+        self.adjacencyList = dict()
+        """
+        Graph:
+               /        bbc       \
+           / news \            / sport \              
+        war    corona   Football    Cricket         
+         |1     |3       |5          |6
+         |2     |4            
+
+        adjancencyList = [
+            // bbc is the base url
+            bbc : [news, sport]
+            news : [war, corona]
+            sport : [Football, Cricket]
+            war : [1, 2]
+            corona : [3,4]
+            Football : [5]
+            cricket : [6]
+            // 1 to 6 are the end nodes
+            1 : []
+            2 : []
+            3 : []
+            4 : []
+            5 : []
+            6 : []
+        ]
+        """
 
     def crawl_categories(self):
         """
@@ -22,11 +47,10 @@ class BBC:
         """
         categories = self.soup.find_all(
             "a", href=re.compile(r"bbc\.com")
-        )[2:6]
+        )[2:7]
         self.adjacencyList[self.base_url] = list()
-        # finds all anger tags with bbc.com in href
+        # finds all anker tags with bbc.com in href
         end_nodes = dict()
-        # enn_nodes is used to add the subcategories as a node in adacencyList
         for category in categories:
             category_link = category["href"]
             category_text = category.span.text.lower()
@@ -34,10 +58,6 @@ class BBC:
                 category_link, category_text
             )
             self.adjacencyList[category_link] = subcategories
-            """ here the list of categories is appended to base url base url the 1st node
-            and the subcategories its neigbours
-            """
-
             self.adjacencyList[self.base_url].append(
                 category_link
             )
@@ -47,12 +67,15 @@ class BBC:
         self.adjacencyList.update(end_nodes)
 
     def bfs(self):
-        visited = set()
+        visited = dict()
+        for key in self.adjacencyList.keys():
+            visited[key] = False
         return_map = dict()
         queue = [self.base_url]
-        visited.add(self.base_url)
+        visited[self.base_url] = True
         while queue:
             link = queue.pop(0)
+            print(link)
             if len(self.adjacencyList[link]) < 1:
                 """Check if the link is the end node. i.e subcategory"""
                 headlines = get_headlines(link)
@@ -60,8 +83,7 @@ class BBC:
                     return_map[link] = headlines
 
             for child in self.adjacencyList.get(link):
-                queue.append(child)
-                if child not in visited:
-                    visited.add(child)
+                if not visited[child]:
                     queue.append(child)
+                    visited[child] = True
         return return_map
--- crawl.py
+++ crawl.py
@@ -3,19 +3,20 @@ import requests
 import re
 
 
-def crawl_sub_categories(cat_link, cat_name):
+def crawl_sub_categories(category_link, category_name):
     """ crawls the cat_link to return a list containing
     the links of different subcategories
     """
 
-    html_doc = requests.get(cat_link)
+    html_doc = requests.get(category_link)
     soup = BeautifulSoup(html_doc.text, 'html.parser')
-    subcat = soup.find_all("a", {"href": re.compile(f"/{cat_name}")})[:10]
+    """Find anker tags with /category_name because everysubcategory 
+    is /category_name/additional_path"""
+    subcat = soup.find_all("a", {"href": re.compile(f"/{category_name}")})[:10]
     subcat = [i for i in subcat if i]
 
-
     def is_short(string): return bool(len(string.split("/")) == 3)
-    def join_base(path): return cat_link.split(cat_name)[0][:-1]+path
+    def join_base(path): return category_link.split(category_name)[0][:-1]+path
 
     subcat1 = list()
     for i in subcat:
